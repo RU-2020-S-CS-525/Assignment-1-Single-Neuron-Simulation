87db3939e0bcaeaeb8248b1d72ad1bc58700ff8f
endlesstory <603907639@qq.com>
2020-02-16 20:24:08 -0500

    update Q5

--- code/hw1.py
+++ code/hw1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 class LIF(object):
     #leaky integrate-and-fire model
     #C_m \frac{dV}{dt} = I(t) - frac{V_m (t)}{R_m}
-    def __init__(self, capitance = 1, resistance = 20, vRest = -65, vThreshold = -40, dt = 0.01, leaky = True):
+    def __init__(self, capitance = 1, resistance = 20, vRest = -65, vThreshold = 5, dt = 0.01, leaky = True):
         #float capitance: C_m
         #float resistance: R_m
         #float vRest: rest votage V_r
@@ -190,7 +190,80 @@ class HodgkinHuxley(object):
         self.dt = dt
         self.TTX = TTX
         self.pronase = pronase
+
+        self.vRest = 0
+        self.mInit = 0.05
+        self.hInit = 0.60
+        self.nInit = 0.32
         return
+
+    def _update(self, tempCurrent, tempVotage, tempM, tempH, tempN):
+        #IN
+        #np.ndarray tempCurrent, dtype = np.float64, shape = (1, n): n different input currents
+        #np.ndarray tempVotage, dtype = np.float64, shape = (1, n): n different membrance potentials
+        #np.ndarray tempM, dtype = np.float64, shape = (1, n): n different m values
+        #np.ndarray tempH, dtype = np.float64, shape = (1, n): n different h values
+        #np.ndarray tempN, dtype = np.float64, shape = (1, n): n different n values
+        #OUT
+        #np.ndarray tempVotage, dtype = np.float64, shpape = (1, n): updated membrance potential
+        #np.ndarray tempM, dtype = np.float64, shape = (1, n): updated m values
+        #np.ndarray tempH, dtype = np.float64, shape = (1, n): updated h values
+        #np.ndarray tempN, dtype = np.float64, shape = (1, n): updated n values
+        #np.ndarray spike, dtype = np.bool, shpape = (1, n): True: fire; False: not fire
+
+        aM = 0.1 * (25 - tempVotage) / (np.exp((25 - tempVotage) / 10) - 1)
+        bM = 4 * np.exp(-1 * tempVotage / 18)
+        aH = 0.07 * np.exp(-1 * tempVotage / 20)
+        bH = 1 / (np.exp((30 - tempVotage) / 10) + 1)
+        aN = 0.01 * (10 - tempVotage) / (np.exp(10 - tempVotage) / 10 - 1)
+        bN = 0.125 * np.exp(-1 * tempVotage / 80)
+
+        dV = (tempCurrent - self.gK * np.power(tempN, 4) * (tempVotage - self.VK) - self.gNa * np.power(tempM, 3) * tempH * (tempVotage - self.VNa) - self.gL * (tempVotage - self.VL)) / self.capitance * self.dt
+        dM = (aM * (1 - tempM) - bM * tempM) * self.dt
+        dH = (aH * (1 - tempH) - bH * tempH) * self.dt
+        dN = (aN * (1 - tempN) - bN * tempN) * self.dt
+
+        tempVotage = tempVotage + dV
+        tempM = tempM + dM
+        tempH = tempH + dH
+        tempN = tempN + dN
+        return tempVotage, tempM, tempH, tempN
+
+    def simulate(self, current):
+        #IN
+        #np.ndarray current, dtype = np.float64, shape = (k, n): input current, |t| = k * dt, n different currents
+        #OUT
+        #np.ndarray votage, dtype = np.float64, shape = (k, n): membrance potential
+        self.votage = np.empty_like(current, dtype = np.float64)
+        self.stepNum, self.simulationNum = current.shape
+
+        #init v, u
+        tempVotage = np.full((1, self.simulationNum), self.vRest, dtype = np.float64)
+        tempM = np.full((1, self.simulationNum), self.mInit, dtype = np.float64)
+        tempH = np.full((1, self.simulationNum), self.hInit, dtype = np.float64)
+        tempN = np.full((1, self.simulationNum), self.nInit, dtype = np.float64)
+        #loop
+        for i in range(self.stepNum):
+            self.votage[i], tempM, tempH, tempN = self._update(current[i], tempVotage, tempM, tempH, tempN)
+            tempVotage = self.votage[i]
+        return self.votage
+
+    def plot(self, currentList):
+        #list currentList: [float current]
+        color = ['b', 'g', 'r', 'c', 'm', 'y']
+        if self.simulationNum > len(color):
+            print('E: too many currents')
+            exit(-1)
+
+        time = np.array(range(self.stepNum), dtype = np.float64) * self.dt
+        for i in range(self.simulationNum):
+            line, = plt.plot(time, self.votage[:, i], c = color[i])
+            line.set_label('I = ' + str(currentList[i]) + 'mA')
+        plt.xlabel('time (msec)')
+        plt.ylabel('votage (mV)')
+        plt.legend(loc = 2)
+        plt.show()
+        return    
         
 
 def Q1(currentList, timeWindow, capitance, resistance, vThreshold, vRest, dt = 0.01, leaky = True):
@@ -272,14 +345,42 @@ def Q4(currentList, timeWindow, a = 0.02, b = 0.2, c = -65, d = 2, vThreshold = 
     izhikevich.plot(currentList)
     return
 
+def Q5(currentList, timeWindow, capitance = 1, gK = 36, gNa = 120, gL = 0.3, VK = -12, VNa = 115, VL = 10.6, dt = 0.01, TTX = False, pronase = False):
+    #IN
+    #list currentList: [float current]
+    #float timeWindow: simulation time
+    #float capitance: C_m
+    #float resistance: R_m
+    #float vThreshold: threshold votage V_t
+    #float vRest: rest votage V_r
+    #float dt: simulation step size, msec
+    #bool leaky: True: leaky integrate-and-fire model; False: leaky-free integrate-and-fire model 
+
+    #preprocessing
+    stepNum = int(np.ceil(timeWindow / dt))
+    simulationNum = len(currentList)
+
+    #init input current
+    current = np.empty((stepNum, simulationNum), dtype = np.float64)
+    for i in range(simulationNum):
+        current[:, i] = currentList[i]
+
+    #init LIF model
+    HH = HodgkinHuxley(capitance, gK, gNa, gL, VK, VNa, VL, dt, TTX, pronase)
+
+    #simulate and plot
+    HH.simulate(current)
+    HH.plot(currentList)
+    return
+
 if __name__ == '__main__':
-    currentList = [0.3, 0.4, 0.5]
-    timeWindow = 1000
-    capitance = 1
-    resistance = 20
-    vRest = -65
-    vThreshold = 5
-    Q1(currentList, timeWindow, capitance, resistance, vRest, vThreshold, dt = 0.01, leaky = True)
+    # currentList = [0.3, 0.4, 0.5]
+    # timeWindow = 1000
+    # capitance = 1
+    # resistance = 20
+    # vRest = -65
+    # vThreshold = 5
+    # Q1(currentList, timeWindow, capitance, resistance, vRest, vThreshold, dt = 0.01, leaky = True)
 
     # minCurrent = 0.1
     # maxCurrent = 1
@@ -294,3 +395,14 @@ if __name__ == '__main__':
     # d = 2
     # vThreshold = 30
     # Q4(currentList, timeWindow, a, b, c, d, vThreshold, dt = 0.01)
+
+    currentList = [30, 50, 80]
+    timeWindow = 25
+    capitance = 1
+    gK = 36
+    gNa = 120
+    gL = 0.3
+    VK = -12
+    VNa = 115
+    VL = 10.6
+    Q5(currentList, timeWindow, capitance, gK, gNa, gL, VK, VNa, VL, dt = 0.01, TTX = False, pronase = False)
