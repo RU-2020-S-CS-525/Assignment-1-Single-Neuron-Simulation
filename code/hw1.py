74a3252a1eda50f76667c0a782423d7d986b6fc7
endlesstory <603907639@qq.com>
2020-02-16 21:34:07 -0500

    update Q6

--- code/hw1.py
+++ code/hw1.py
@@ -86,6 +86,8 @@ class Izhikevich(object):
         #float b: sensitivity of u to v
         #float c: after-spike reset v
         #float d: after-spike reset u
+        #float vThreshold: threshold votage V_t
+        #float dt: simulation step size, msec
         super(Izhikevich, self).__init__()
         self.a = a
         self.b = b
@@ -179,6 +181,16 @@ class HodgkinHuxley(object):
     #a_n(V) = \frac{0.01 (10 - V)}{\exp(10 - V) - 1}
     #b_n(V) = 0.125 \exp(\frac{-V}{80})
     def __init__(self, capitance = 1, gK = 36, gNa = 120, gL = 0.3, VK = -12, VNa = 115, VL = 10.6, dt = 0.01, TTX = False, pronase = False):
+        #float capitance: C_m
+        #float gK: maximum conductance for K
+        #float gNa: maximum conductance for Na
+        #float gL: maximum conductance for other linear ions
+        #float VK: equilibrium potential for K
+        #flaot VNa: equilibrium potential for Na
+        #float VL: equilibrium potential for other linear ions
+        #float dt: simulation step size, msec
+        #bool TTX: True: use drug TTX; False: not use
+        #bool pronase: True: use drug pronase; False: not use
         super(HodgkinHuxley, self).__init__()
         self.capitance = capitance
         self.gK = gK
@@ -191,6 +203,11 @@ class HodgkinHuxley(object):
         self.TTX = TTX
         self.pronase = pronase
 
+        #init cell
+        #float vRest: rest votage V_r
+        #float mInit: gating variable for Na activation gate
+        #float hInit: gating variable for Na inactivation gate
+        #float nInit: gating variable for K activation gate
         self.vRest = 0
         self.mInit = 0.05
         self.hInit = 0.60
@@ -211,22 +228,62 @@ class HodgkinHuxley(object):
         #np.ndarray tempN, dtype = np.float64, shape = (1, n): updated n values
         #np.ndarray spike, dtype = np.bool, shpape = (1, n): True: fire; False: not fire
 
+
+        #compute a, b
         aM = 0.1 * (25 - tempVotage) / (np.exp((25 - tempVotage) / 10) - 1)
         bM = 4 * np.exp(-1 * tempVotage / 18)
-        aH = 0.07 * np.exp(-1 * tempVotage / 20)
-        bH = 1 / (np.exp((30 - tempVotage) / 10) + 1)
+        if self.pronase:
+            tempH = 1
+        else:
+            aH = 0.07 * np.exp(-1 * tempVotage / 20)
+            bH = 1 / (np.exp((30 - tempVotage) / 10) + 1)
         aN = 0.01 * (10 - tempVotage) / (np.exp(10 - tempVotage) / 10 - 1)
         bN = 0.125 * np.exp(-1 * tempVotage / 80)
 
-        dV = (tempCurrent - self.gK * np.power(tempN, 4) * (tempVotage - self.VK) - self.gNa * np.power(tempM, 3) * tempH * (tempVotage - self.VNa) - self.gL * (tempVotage - self.VL)) / self.capitance * self.dt
+        #update V, m, h, n
+        IK = self.gK * np.power(tempN, 4) * (tempVotage - self.VK)
+        if self.TTX:
+            INa = 0
+        else:
+            INa = self.gNa * np.power(tempM, 3) * tempH * (tempVotage - self.VNa)
+        IL = self.gL * (tempVotage - self.VL)
+        dV = (tempCurrent - IK - INa - IL) / self.capitance * self.dt
         dM = (aM * (1 - tempM) - bM * tempM) * self.dt
-        dH = (aH * (1 - tempH) - bH * tempH) * self.dt
+        if self.pronase:
+            pass
+        else:
+            dH = (aH * (1 - tempH) - bH * tempH) * self.dt
         dN = (aN * (1 - tempN) - bN * tempN) * self.dt
-
         tempVotage = tempVotage + dV
         tempM = tempM + dM
-        tempH = tempH + dH
+        if self.pronase:
+            tempH = 1
+        else:
+            tempH = tempH + dH
         tempN = tempN + dN
+
+        #avoid overflow
+        mask0 = tempM < 0
+        mask1 = tempM > 1
+        if mask0.any() or mask1.any():
+            print('W: overflow for m')
+            tempM[mask0] = 0
+            tempM[mask1] = 1
+        if self.pronase:
+            pass
+        else:
+            mask0 = tempH < 0
+            mask1 = tempH > 1
+            if mask0.any() or mask1.any():
+                print('W: overflow for h')
+                tempH[mask0] = 0
+                tempH[mask1] = 1
+        mask0 = tempN < 0
+        mask1 = tempN > 1
+        if mask0.any() or mask1.any():
+            print('W: overflow for n')
+            tempN[mask0] = 0
+            tempN[mask1] = 1
         return tempVotage, tempM, tempH, tempN
 
     def simulate(self, current):
@@ -264,7 +321,7 @@ class HodgkinHuxley(object):
         plt.legend(loc = 2)
         plt.show()
         return    
-        
+
 
 def Q1(currentList, timeWindow, capitance, resistance, vThreshold, vRest, dt = 0.01, leaky = True):
     #IN
@@ -295,6 +352,16 @@ def Q1(currentList, timeWindow, capitance, resistance, vThreshold, vRest, dt = 0
     return
 
 def Q2(minCurrent, maxCurrent, currentStepSize, timeWindow, capitance, resistance, vThreshold, vRest, dt = 0.01, leaky = True):
+    #IN
+    #list currentList: [float current]
+    #float timeWindow: simulation time
+    #float capitance: C_m
+    #float resistance: R_m
+    #float vThreshold: threshold votage V_t
+    #float vRest: rest votage V_r
+    #float dt: simulation step size, msec
+    #bool leaky: True: leaky integrate-and-fire model; False: leaky-free integrate-and-fire model 
+
     #preprocessing
     stepNum = int(np.ceil(timeWindow / dt))
     simulationNum = int(np.ceil((maxCurrent - minCurrent) / currentStepSize))
@@ -316,17 +383,18 @@ def Q2(minCurrent, maxCurrent, currentStepSize, timeWindow, capitance, resistanc
     plt.xlabel('current (mA)')
     plt.ylabel('firing rate (Hz)')
     plt.show()
+    return
 
 def Q4(currentList, timeWindow, a = 0.02, b = 0.2, c = -65, d = 2, vThreshold = 30, dt = 0.01):
     #IN
     #list currentList: [float current]
     #float timeWindow: simulation time
-    #float capitance: C_m
-    #float resistance: R_m
+    #float a: time scale of u
+    #float b: sensitivity of u to v
+    #float c: after-spike reset v
+    #float d: after-spike reset u
     #float vThreshold: threshold votage V_t
-    #float vRest: rest votage V_r
     #float dt: simulation step size, msec
-    #bool leaky: True: leaky integrate-and-fire model; False: leaky-free integrate-and-fire model 
 
     #preprocessing
     stepNum = int(np.ceil(timeWindow / dt))
@@ -337,7 +405,7 @@ def Q4(currentList, timeWindow, a = 0.02, b = 0.2, c = -65, d = 2, vThreshold = 
     for i in range(simulationNum):
         current[:, i] = currentList[i]
 
-    #init LIF model
+    #init Izhikevich model
     izhikevich = Izhikevich(a, b, c, d, vThreshold, dt)
 
     #simulate and plot
@@ -350,11 +418,15 @@ def Q5(currentList, timeWindow, capitance = 1, gK = 36, gNa = 120, gL = 0.3, VK 
     #list currentList: [float current]
     #float timeWindow: simulation time
     #float capitance: C_m
-    #float resistance: R_m
-    #float vThreshold: threshold votage V_t
-    #float vRest: rest votage V_r
+    #float gK: maximum conductance for K
+    #float gNa: maximum conductance for Na
+    #float gL: maximum conductance for other linear ions
+    #float VK: equilibrium potential for K
+    #flaot VNa: equilibrium potential for Na
+    #float VL: equilibrium potential for other linear ions
     #float dt: simulation step size, msec
-    #bool leaky: True: leaky integrate-and-fire model; False: leaky-free integrate-and-fire model 
+    #bool TTX: True: use drug TTX; False: not use
+    #bool pronase: True: use drug pronase; False: not use
 
     #preprocessing
     stepNum = int(np.ceil(timeWindow / dt))
@@ -365,13 +437,54 @@ def Q5(currentList, timeWindow, capitance = 1, gK = 36, gNa = 120, gL = 0.3, VK 
     for i in range(simulationNum):
         current[:, i] = currentList[i]
 
-    #init LIF model
+    #init HH model
     HH = HodgkinHuxley(capitance, gK, gNa, gL, VK, VNa, VL, dt, TTX, pronase)
 
     #simulate and plot
     HH.simulate(current)
     HH.plot(currentList)
     return
+
+def Q6(current, timeWindow, capitance = 1, gK = 36, gNa = 120, gL = 0.3, VK = -12, VNa = 115, VL = 10.6, dt = 0.01):
+    #IN
+    #float current: input current
+    #float timeWindow: simulation time
+    #float capitance: C_m
+    #float gK: maximum conductance for K
+    #float gNa: maximum conductance for Na
+    #float gL: maximum conductance for other linear ions
+    #float VK: equilibrium potential for K
+    #flaot VNa: equilibrium potential for Na
+    #float VL: equilibrium potential for other linear ions
+    #float dt: simulation step size, msec
+
+    #preprocessing
+    stepNum = int(np.ceil(timeWindow / dt))
+    current = np.full((stepNum, 1), current, dtype = np.float64)
+
+    #init HH model
+    HH0 = HodgkinHuxley(capitance, gK, gNa, gL, VK, VNa, VL, dt, TTX = False, pronase = False)
+    HH1 = HodgkinHuxley(capitance, gK, gNa, gL, VK, VNa, VL, dt, TTX = True, pronase = False)
+    HH2 = HodgkinHuxley(capitance, gK, gNa, gL, VK, VNa, VL, dt, TTX = False, pronase = True)
+
+    #simulate
+    v0 = HH0.simulate(current)
+    v1 = HH1.simulate(current)
+    v2 = HH2.simulate(current)
+
+    time = np.array(range(stepNum), dtype = np.float64) * dt
+
+    line, = plt.plot(time, v0, c = 'b')
+    line.set_label('no drug')
+    line, = plt.plot(time, v1, c = 'g')
+    line.set_label('TTX')
+    line, = plt.plot(time, v2, c = 'r')
+    line.set_label('pronase')
+    plt.xlabel('time (msec)')
+    plt.ylabel('votage (mV)')
+    plt.legend(loc = 2)
+    plt.show()
+
 
 if __name__ == '__main__':
     # currentList = [0.3, 0.4, 0.5]
@@ -396,7 +509,18 @@ if __name__ == '__main__':
     # vThreshold = 30
     # Q4(currentList, timeWindow, a, b, c, d, vThreshold, dt = 0.01)
 
-    currentList = [30, 50, 80]
+    # currentList = [-1, 5, 9]
+    # timeWindow = 25
+    # capitance = 1
+    # gK = 36
+    # gNa = 120
+    # gL = 0.3
+    # VK = -12
+    # VNa = 115
+    # VL = 10.6
+    # Q5(currentList, timeWindow, capitance, gK, gNa, gL, VK, VNa, VL, dt = 0.01, TTX = False, pronase = False)
+
+    current = 5
     timeWindow = 25
     capitance = 1
     gK = 36
@@ -405,4 +529,4 @@ if __name__ == '__main__':
     VK = -12
     VNa = 115
     VL = 10.6
-    Q5(currentList, timeWindow, capitance, gK, gNa, gL, VK, VNa, VL, dt = 0.01, TTX = False, pronase = False)
+    Q6(current, timeWindow, capitance, gK, gNa, gL, VK, VNa, VL, dt = 0.01)
